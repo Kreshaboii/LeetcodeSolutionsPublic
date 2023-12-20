9006076f50fc57112702b10fdfd0bd7f1bef5625
Kreshaboii <kreshnikmustafa2005@gmail.com>
2023-12-21 00:10:12 +0100

    Create Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,97 @@
+//Build Array from Permutation
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i] + (n * (nums[nums[i]] % n));
+        }
+
+        for(int i = 0;i < n; i++){
+            nums[i] /= n;
+        }
+
+        return nums;
+    }
+};
+
+
+//Number of Good Pairs
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int n = nums.size();
+        int pairs = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < n; j++){
+                if(nums[i] == nums[j] && i < j){
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+};
+
+
+//How Many Numbers Are Smaller Than the Current Number
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> returnVector;
+
+        for(int i = 0; i < n; i++){
+            returnVector.push_back(0);
+        } 
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(nums[j] != nums[i] && nums[j] < nums[i]){
+                    returnVector[i] += 1;
+                }
+            }
+        }
+        return returnVector;
+    }
+};
+
+
+//Count Pairs Whose Sum is Less than Target
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int pairs = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(nums[i] + nums[j] < target){
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+};
+
+
+//Left and Right Sum Differences
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        int right = accumulate(begin(nums), end(nums), 0); //Duke perdorur #include <numeric>
+        vector<int> ans;
+
+        for(int i = 0, left = 0; i < n; i++){
+            right -= nums[i];
+            ans.push_back(abs(left - right));
+            left += nums[i]; 
+        }
+        return ans;
+    }
+};
